f2c9a80df353193d64adf6b039cd5e02d03ae2cd
Qianruipku <terry_liu@pku.edu.cn>
2025-08-08 21:11:43 +0100

    change default sample_all

--- ferminet/base_config.py
+++ ferminet/base_config.py
@@ -240,7 +240,7 @@ def default() -> ml_collections.ConfigDict:
           'scale_by_nuclear_distance': False,
           'blocks': 1,  # Number of blocks to split the MCMC sampling into
           'fake_energy': False,  # If true, use a fake energy to sample
-          'sample_all': True,  # If true, sample all particles in each step. If False, sample only one particle at a time.
+          'sample_all': False,  # If true, sample all particles in each step. If False, sample only one particle at a time.
       },
       'network': {
           'network_type': 'ferminet',  # One of 'ferminet' or 'psiformer'.
